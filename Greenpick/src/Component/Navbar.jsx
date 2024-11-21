f289335fe01c4fbab4cc5477f63af3ed19bc1150
7anish <anissh946@gmail.com>
2024-11-22 03:23:00 +0530

    rates

--- Greenpick/src/Component/Navbar.jsx
+++ Greenpick/src/Component/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
                 <div class="px-4 sm:px-6 lg:px-8">
                     <div class="w-full flex  flex-col lg:flex-row lg:items-center lg:justify-between">
                         <div class=" flex justify-between  lg:flex-row">
-                            <a href="javascript:;" class="flex items-center">
+                            <NavLink to={'/'} class="flex items-center">
                             <img src={logo} alt="" className='w-24 h-20' />
-                            </a>
+                            </NavLink>
                             <button data-collapse-toggle="navbar-default" type="button"
                                 class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
                                 aria-controls="navbar-default" aria-expanded="false">
@@ -26,19 +26,19 @@ const Navbar = () => {
                         <div class="hidden w-full lg:flex lg:pl-11 " id="navbar-default">
                             <ul class="lg:flex items-center flex-col mt-4 lg:mt-0 lg:flex-row lg:ml-auto gap-4">
                                 <li>
-                                    <NavLink to={'/'} class="nav-link mb-2 block lg:mr-6 md:mb-0 lg:text-left text-center">Home</NavLink>
+                                    <NavLink to={'/'} class="nav-link mb-2 block lg:mr-6 md:mb-0 lg:text-left text-center font-medium">Home</NavLink>
                                 </li>
                                 <li>
-                                    <NavLink to={'/pricing'} class="nav-link mb-2 block lg:mr-6 md:mb-0 lg:text-left text-center">Pricing</NavLink>
+                                    <NavLink to={'/rates'} class="nav-link mb-2 block lg:mr-6 md:mb-0 lg:text-left text-center font-medium">Rates</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={'/pricing'} class="nav-link mb-2 block lg:mr-6 md:mb-0 lg:text-left text-center font-medium">Pricing</NavLink>
                                 </li>
                             </ul>
                             <div class="flex items-center ml-5 justify-center">
                                 <button
                                     class=" bg-green-50 text-green-600 rounded-full cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 py-3 px-6 text-sm hover:bg-green-100">Login
                                 </button>
-                                <button
-                                    class="bg-green-600 text-white rounded-full cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 py-3 px-6 text-sm md:ml-5 hover:bg-green-700">Sign
-                                    up </button>
                             </div>
                         </div>
                     </div>
