91b93d74ac5cf15ce217d8e460efe43bb00da283
Atulya Rounak <84398957+realaboveall@users.noreply.github.com>
2024-11-30 08:42:12 +0530

    Update Chatbot.jsx

--- Greenpick/src/Component/Chatbot.jsx
+++ Greenpick/src/Component/Chatbot.jsx
@@ -21,7 +21,7 @@ const Chatbox = () => {
         setMessages((prevMessages) => [...prevMessages, userMessage]);
 
         try {
-            const response = await fetch('https://8f89-59-145-191-138.ngrok-free.app/predict', {
+            const response = await fetch('https://8100-59-145-191-138.ngrok-free.app/predict', {
                 method: 'POST',
                 body: JSON.stringify({ message: text }),
                 mode: 'cors',
